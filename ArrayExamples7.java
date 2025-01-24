208bf2523778273f68a2b3598df6218bf0090811
skandhwa <113922787+skandhwa@users.noreply.github.com>
2025-01-24 20:31:10 +0530

    Add files via upload

---
+++ ArrayExamples7.java
@@ -0,0 +1,28 @@
+package TestJanbask;
+
+import java.util.Arrays;
+
+public class ArrayExamples7 {
+
+	public static void main(String[] args) {
+		
+		String []a= {"Selenium","UFT","Python","Java"};
+		int x=a.length;
+		System.out.println(x);
+		
+	String str1=	Arrays.toString(a);
+	
+	System.out.println(str1);
+	
+	boolean p=Arrays.asList(a).contains("java");
+	System.out.println(p);
+	
+	
+	
+		
+		
+		
+
+	}
+
+}
